fef2f892e066c4736d0e101be653f4c7ac294906
kosovic-drasko <kosovic.drasko@montefarmco.me>
2023-03-22 09:57:13 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VideoModule } from "./video/video.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        VideoModule
+    ]
 })
 export class AppModule { }
